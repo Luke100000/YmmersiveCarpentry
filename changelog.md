c494d42d0c11b8e8bf5b01368b5e0ec48a1a1a25
Luke100000 <jonas.peche@aon.at>
2026-01-28 21:25:48 +0100

    integrated Resource Groups

--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+# 1.1.0
+
+* Integrated Resource Groups to allow better crafting recipes (Now works with planks or trunks)
+* Added resource groups to variants (now counts as planks, fuel, ...)
+
 # 1.0.1
 
 * Fixed stairs
--- src/main/java/net/conczin/YmmersiveCarpentry.java
+++ src/main/java/net/conczin/YmmersiveCarpentry.java
@@ -1,25 +1,31 @@
 package net.conczin;
 
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.plugin.PluginBase;
+import com.hypixel.hytale.server.core.plugin.PluginManager;
 
 import javax.annotation.Nonnull;
+import java.awt.*;
 
 
 public class YmmersiveCarpentry extends JavaPlugin {
-    private static YmmersiveCarpentry instance;
-
     public YmmersiveCarpentry(@Nonnull JavaPluginInit init) {
         super(init);
-        instance = this;
     }
 
     @Override
     protected void setup() {
-
+        PluginBase plugin = PluginManager.get().getPlugin(PluginIdentifier.fromString("Conczin:Resource Groups"));
+        if (plugin == null) {
+            getEventRegistry().registerGlobal(PlayerReadyEvent.class, this::onPlayerConnect);
+        }
     }
 
-    public static YmmersiveCarpentry getInstance() {
-        return instance;
+    private void onPlayerConnect(PlayerReadyEvent event) {
+        event.getPlayer().sendMessage(Message.translation("server.YmmersiveCarpentry.resourceGroupsMissing").color(Color.ORANGE));
     }
 }
